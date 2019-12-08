d42fb70f299fd72465ffc0ccedbc1c3ef9b0e724
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-12-08 14:10:04 -0800

    add func get_map_size

--- map.c
+++ map.c
@@ -6,19 +6,18 @@
 /*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/08 13:08:51 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/12/08 13:11:55 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/12/08 14:08:31 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <stdio.h> // TODO DELETE THAT LINE!
 
 /*
 ** Function to connect lines to each other
 ** Return line
 */
 
-char		*join_lines(char *line, char *prevlines)
+static char		*join_lines(char *line, char *prevlines)
 {
 	char		*temp;
 
@@ -29,25 +28,44 @@ char		*join_lines(char *line, char *prevlines)
 }
 
 /*
-** Function to connect lines to each other
+** Function to read and check map file
 */
 
-char			*check_map_file(char *filename)
+int				get_map_size(char *filename, t_map *current_map)
 {
 	int			fd;
 	char		*line;
-	char		*result;
+	int			height;
+	int			width;
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 2)
-		return (NULL);
-
-	result = ft_strnew(0);
+		return (0);
+	height = 0;
+	width = 0;
 	while (ft_get_next_line(fd, &line) > 0)
 	{
-		result = join_lines(line, result);
+		if (!width)
+			width = ft_count_words(line, ' ');
+		if (width != ft_count_words(line, ' '))
+			return (0);
+		height++;
 	}
-	printf("%s\n", result);  // TODO DELETE THAT LINE!
+	current_map->height = height;
+	current_map->width = width;
 	close(fd);
-	return (result);
+	ft_strdel(&line);
+	return(1);
+}
+
+/*
+** Function to read and check map file
+*/
+
+int				read_map_file(char *filename, t_map *current_map)
+{
+
+	get_map_size(filename, current_map);
+
+	return (1);
 }
