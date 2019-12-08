7cda33396135ab7ba41a36e1bc06a29a064eef10
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-12-08 15:43:47 -0800

    rename var current_map to map

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/08 13:08:51 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/12/08 15:24:06 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/12/08 15:31:11 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ char		*join_lines(char *line, char *prevlines)
 ** Function to read and check map file
 */
 
-int				get_map_size(char *filename, t_map *current_map)
+int				get_map_size(char *filename, t_map *map)
 {
 	int			fd;
 	char		*line;
@@ -53,8 +53,8 @@ int				get_map_size(char *filename, t_map *current_map)
 		height++;
 		ft_strdel(&line);
 	}
-	current_map->height = height;
-	current_map->width = width;
+	map->height = height;
+	map->width = width;
 	close(fd);
 	return (1);
 }
@@ -83,19 +83,19 @@ void				save_map_data(int *z_row, char *line)
 ** Function to get map data
 */
 
-int				get_map_data(char *filename, t_map *current_map)
+int				get_map_data(char *filename, t_map *map)
 {
 	int			fd;
 	int			i;
 	char		*line;
 
-	if (!(current_map->z_data = (int **)malloc(sizeof(int*)
-	* (current_map->height + 1))))
+	if (!(map->z_data = (int **)malloc(sizeof(int*)
+	* (map->height + 1))))
 		return (0);
 	i = 0;
-	while (i <= current_map->height)
-		if (!(current_map->z_data[i++] = (int*)malloc(sizeof(int)
-		* (current_map->width + 1))))
+	while (i <= map->height)
+		if (!(map->z_data[i++] = (int*)malloc(sizeof(int)
+		* (map->width + 1))))
 			return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 2)
@@ -103,7 +103,7 @@ int				get_map_data(char *filename, t_map *current_map)
 	i = 0;
 	while (ft_get_next_line(fd, &line) > 0)
 	{
-		save_map_data(current_map->z_data[i++], line);
+		save_map_data(map->z_data[i++], line);
 		ft_strdel(&line);
 	}
 	close(fd);
@@ -114,9 +114,9 @@ int				get_map_data(char *filename, t_map *current_map)
 ** Function to read and check map file
 */
 
-int				read_map_file(char *filename, t_map *current_map)
+int				read_map_file(char *filename, t_map *map)
 {
-	get_map_size(filename, current_map);
-	get_map_data(filename, current_map);
+	get_map_size(filename, map);
+	get_map_data(filename, map);
 	return (1);
 }
