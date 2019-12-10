b7dd444e7202b1718bf056ca1cdfe86457e884dc
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-12-09 20:10:14 -0800

    delete debug vars

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/08 13:08:51 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/12/09 19:25:09 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/12/09 20:09:12 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,20 @@ int				get_map_size(char *filename, t_map *map)
 	char		*line;
 	int			height;
 	int			width;
-	int			test; //TODO delete this line
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 2)
 		return (0);
 	height = 0;
 	width = 0;
-	test = 0; // TODO delete
 	while (ft_get_next_line(fd, &line) > 0)
 	{
 		if (!width)
 			width = ft_count_words(line, ' ');
-		test = ft_count_words(line, ' '); // TODO delete this line
 		if (width != ft_count_words(line, ' '))
+		{
 			return (0);
-		printf("%s\n", line); // TODO DELETE THIS LINE
+		}
 		height++;
 		ft_strdel(&line);
 	}
@@ -93,7 +91,6 @@ int				get_map_data(char *filename, t_map *map)
 	i = 0;
 	while (ft_get_next_line(fd, &line) > 0)
 	{
-		printf("read line: %i\n", i);// TODO Delete this line
 		save_map_data(map->z_data[i++], line);
 		ft_strdel(&line);
 	}
@@ -108,7 +105,6 @@ int				get_map_data(char *filename, t_map *map)
 int				read_map_file(char *filename, t_map *map)
 {
 	get_map_size(filename, map);
-	printf("%i\t%i\n", map->height, map->width); //TODO delete this line;
 	get_map_data(filename, map);
 	return (1);
 }
